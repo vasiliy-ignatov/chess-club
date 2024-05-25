ba04387939a6d6aaecfad59bc7932771ec819bb2
Vasiliy Ignatov <inaburat@gmail.com>
2024-05-25 22:55:56 +0300

    fonts added

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -3,3 +3,27 @@
 $("document").ready(function() {
 	console.log('hello world');
 });
+
+
+const swiper = new Swiper('.swiper', {
+	slidesPerView: 1,
+	autoHeight: true,
+    effect: 'coverflow',
+	loop: true,
+	// pagination: {
+	// 	el: '.product-swiper__pagination',
+	// },
+	breakpoints: {
+	  768: {
+		slidesPerView: 1,
+        slidesPerGroup: 1,
+		autoHeight: false,
+		effect: 'slide'
+	  },
+	  1180: {
+		slidesPerView: 5,
+		autoHeight: false,
+		effect: 'slide'
+	  }
+	}
+});
